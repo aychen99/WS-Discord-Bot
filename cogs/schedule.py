cbebf1ad0f5cb0e27768c9de003139dbc4722ddc
aychen99 <42681629+aychen99@users.noreply.github.com>
2019-08-19 22:53:39 -0400

    Implement "change" subcommand
    Also separated user input parsing for "set" and "change" subcommands
    into a helper method.

--- cogs/schedule.py
+++ cogs/schedule.py
@@ -84,19 +84,19 @@ class Schedule(commands.Cog):
                                'none already exist, or erases the old '
                                'schedule entirely should the user confirm '
                                'their intent. \n'
-                               'Supported syntax is of the form \n'
-                               '"!schedule set [weekday] [start time] - '
+                               'Supported syntax is of the form '
+                               '```!schedule set [weekday] [start time] - '
                                '[end time], [other weekday] [start time] - '
                                '[end time] and [start time] - [end time], '
-                               '..." \n'
+                               '...```\n'
                                'where start and end times for a single '
                                '"shift" are separated by a dash, '
                                'different "shifts" in a day are separated '
                                'by the "*and*" keyword, and different days '
                                'are separated by a comma. '
                                'This subcommand is also case-insensitive. \n'
-                               'Some examples of supported syntax are as '
-                               'follows: \n'
+                               'Some in-practice examples of supported '
+                               'syntax are as follows: \n'
                                '```'
                                '!schedule set Monday 8 AM - 12 PM\n'
                                '!schedule set Monday 8:00 - 13:00\n'
@@ -105,7 +105,7 @@ class Schedule(commands.Cog):
                                '10:00 - 20:00\n'
                                '!schedule set Tue 8:00 - 12:00 and '
                                '15:00 - 20:00'
-                               '```'
+                               '```\n'
                                '**Currently does not support overnight '
                                'scheduling!**')
                 return
@@ -147,108 +147,9 @@ class Schedule(commands.Cog):
                                  '3' : [], '4' : [], '5' : [],
                                  '6' : []}
             
-            # Parse user input
             try:
-                weekdays = {'sunday': '6', 'monday': '0', 'tuesday': '1', 
-                            'wednesday': '2', 'thursday': '3', 'friday': '4', 
-                            'saturday': '5'}
-                
-                full_message = ((' ').join(args)).lower()
-                input_by_days = [day.strip() 
-                                 for day in full_message.split(',')]
-
-                # Handle each day in user input
-                for day in input_by_days:
-                    day_args = day.split(' ', 1)
-                    current_day = None
-
-                    original_number_of_weekdays = len(weekdays)
-                    inputted_day = day_args[0]
-                    for weekday in weekdays:
-                        if (inputted_day == weekday 
-                                or inputted_day == weekday[0:3]):
-                            current_day = weekdays[weekday]
-                            weekdays.pop(weekday)
-                            break
-                    # disallow mentioning the same day twice
-                    if len(weekdays) == original_number_of_weekdays:
-                        raise ValueError('User did not enter a valid weekday')
-                    
-                    # Handle every set of start and end times within a day
-                    parsed_shift_datetimes = []
-                    times_in_day_string = [time.strip() 
-                                        for time in day_args[1].split('and')]
-                    for time_string in times_in_day_string:
-                        times = [time.strip() 
-                                    for time in time_string.split('-')]
-                        start_and_end_datetimes = []
-                        def parse_time(time_string, format): 
-                            return datetime.datetime.strptime(
-                                                        time_string, format)
-                        for time in times:
-                            # Run through all accepted cases of user input, 
-                            # i.e. "x:xxpm", "x:xx pm", "x pm", and "xx:xx"
-                            try:
-                                start_and_end_datetimes.append(
-                                    parse_time(time, "%I:%M%p"))
-                                continue
-                            except ValueError:
-                                pass
-                            try:
-                                start_and_end_datetimes.append(
-                                    parse_time(time, "%I:%M %p"))
-                                continue
-                            except ValueError:
-                                pass
-                            try:
-                                start_and_end_datetimes.append(
-                                    parse_time(time, "%I%p"))
-                                continue
-                            except ValueError:
-                                pass
-                            try:
-                                start_and_end_datetimes.append(
-                                    parse_time(time, "%I %p"))
-                                continue
-                            except ValueError:
-                                pass
-                            try:
-                                start_and_end_datetimes.append(
-                                    parse_time(time, "%H:%M"))
-                                continue
-                            except ValueError:
-                                pass
-
-                            raise ValueError('Invalid input, could not parse')
-                        
-                        start_datetime = start_and_end_datetimes[0]
-                        end_datetime = start_and_end_datetimes[1]
-                        start_time_in_minutes = (start_datetime.hour*60
-                                                 + start_datetime.minute)
-                        end_time_in_minutes = (end_datetime.hour*60
-                                               + end_datetime.minute)
-                        if (start_time_in_minutes > end_time_in_minutes):
-                            raise ValueError('End time is earlier than start '
-                                             'time')
-
-                        for shift in parsed_shift_datetimes:
-                            other_start = (shift[0].hour*60 
-                                           + shift[0].minute)
-                            other_end = (shift[1].hour*60 
-                                         + shift[1].minute)
-                            if ((end_time_in_minutes > other_start
-                                    and start_time_in_minutes < other_end)
-                                or (start_time_in_minutes < other_end
-                                    and end_time_in_minutes > other_start)):
-                                raise ValueError('Invalid input, as '
-                                                 'scheduled times overlap')
-
-                        parsed_shift_datetimes.append(start_and_end_datetimes)
-
-                    complete_day = [[datetime.datetime.strftime(time, "%H:%M")
-                                     for time in shift]
-                                     for shift in parsed_shift_datetimes]
-                    new_user_schedule[current_day] = complete_day
+                new_user_schedule = self._parse_user_input_times(
+                                        ctx, args, new_user_schedule)
             except ValueError:
                 await ctx.send('Invalid input format. Please try again! You '
                             'can type "!schedule" for more information on '
@@ -262,10 +163,71 @@ class Schedule(commands.Cog):
             await ctx.send('New availability schedule successfully set!')
         elif (subcommand == 'change'):
             if len(args) == 0:
-                await ctx.send('Detailed help message in development! ')
+                await ctx.send('Use this command to change certain days in '
+                               'your schedule, or add shifts for a new day! '
+                               'Uses the same syntax as the "!schedule set" '
+                               'subcommand, i.e.:'
+                               '```!schedule set [weekday] [start time] - '
+                               '[end time], [other weekday] [start time] - '
+                               '[end time] and [start time] - [end time], '
+                               '...```\n'
+                               'where start and end times for a single '
+                               '"shift" are separated by a dash, '
+                               'different "shifts" in a day are separated '
+                               'by the "*and*" keyword, and different days '
+                               'are separated by a comma.\n'
+                               'If there were any old scheduled times in '
+                               'the day(s) that you wish to change, this '
+                               'subcommand will overwrite them completely.')
                 return
-            
-            await ctx.send('TODO') #TODO
+            if (author_id in full_schedule[guild_id]
+                    and full_schedule[guild_id][author_id]):
+                await ctx.send('**WARNING!** You already have a schedule on '
+                               'file. Using the "change" subcommand now will '
+                               'reset all of the days that you '
+                               'have mentioned in your message, if schedule '
+                               'times already exist for them.'
+                               'All of the old information will be deleted, '
+                               'leaving behind only the new times that you '
+                               'have typed in. \n'
+                               'Are you sure you wish to continue with the '
+                               'schedule change? Please type "yes" or "no".')
+                try:
+                    def check(m):
+                        return (m.channel == ctx.message.channel
+                                and m.author == ctx.message.author)
+                    reply = await self.bot.wait_for('message', timeout=60, 
+                                                    check=check)
+                    if reply.content == 'yes':
+                        # Do nothing and continue with schedule change
+                        pass
+                    elif reply.content == 'no':
+                        await ctx.send('Cancelling schedule change. '
+                                       'Please try again once you are ready.')
+                        return
+                    else:
+                        raise ValueError('User typed something other than '
+                                         'yes/no')
+                except (asyncio.TimeoutError, ValueError):
+                    await ctx.send('Invalid input received, cancelling '
+                                   'schedule change. Please try again.')
+                    return
+
+            user_schedule = full_schedule[guild_id][author_id]
+            try:
+                user_schedule = self._parse_user_input_times(
+                                    ctx, args, user_schedule)
+            except ValueError:
+                await ctx.send('Invalid input format. Please try again! You '
+                            'can type "!schedule" for more information on '
+                            'valid formatting.')
+                return
+
+            full_schedule[guild_id][author_id] = user_schedule
+            self._full_schedule = full_schedule
+            with open(f'{self._schedules_json_path}', 'w') as schedule_file:
+                json.dump(full_schedule, schedule_file, indent=4)
+            await ctx.send('Availability schedule successfully changed!')
         elif (subcommand == 'view'):
             if author_id not in full_schedule[guild_id]:
                 await ctx.send('Did not find a schedule on file for you!')
@@ -313,6 +275,109 @@ class Schedule(commands.Cog):
                 await ctx.send('No input received. Cancelling '
                                'schedule clear. Please try again.')
 
+    def _parse_user_input_times(self, ctx, args, user_schedule):
+        weekdays = {'sunday': '6', 'monday': '0', 'tuesday': '1', 
+                    'wednesday': '2', 'thursday': '3', 'friday': '4', 
+                    'saturday': '5'}
+        
+        full_message = ((' ').join(args)).lower()
+        input_by_days = [day.strip() 
+                            for day in full_message.split(',')]
+
+        # Handle each day in user input
+        for day in input_by_days:
+            day_args = day.split(' ', 1)
+            current_day = None
+
+            original_number_of_weekdays = len(weekdays)
+            inputted_day = day_args[0]
+            for weekday in weekdays:
+                if (inputted_day == weekday 
+                        or inputted_day == weekday[0:3]):
+                    current_day = weekdays[weekday]
+                    weekdays.pop(weekday)
+                    break
+            # disallow mentioning the same day twice
+            if len(weekdays) == original_number_of_weekdays:
+                raise ValueError('User did not enter a valid weekday')
+            
+            # Handle every set of start and end times within a day
+            parsed_shift_datetimes = []
+            times_in_day_string = [time.strip() 
+                                for time in day_args[1].split('and')]
+            for time_string in times_in_day_string:
+                times = [time.strip() 
+                            for time in time_string.split('-')]
+                start_and_end_datetimes = []
+                def parse_time(time_string, format): 
+                    return datetime.datetime.strptime(
+                                                time_string, format)
+                for time in times:
+                    # Run through all accepted cases of user input, 
+                    # i.e. "x:xxpm", "x:xx pm", "x pm", and "xx:xx"
+                    try:
+                        start_and_end_datetimes.append(
+                            parse_time(time, "%I:%M%p"))
+                        continue
+                    except ValueError:
+                        pass
+                    try:
+                        start_and_end_datetimes.append(
+                            parse_time(time, "%I:%M %p"))
+                        continue
+                    except ValueError:
+                        pass
+                    try:
+                        start_and_end_datetimes.append(
+                            parse_time(time, "%I%p"))
+                        continue
+                    except ValueError:
+                        pass
+                    try:
+                        start_and_end_datetimes.append(
+                            parse_time(time, "%I %p"))
+                        continue
+                    except ValueError:
+                        pass
+                    try:
+                        start_and_end_datetimes.append(
+                            parse_time(time, "%H:%M"))
+                        continue
+                    except ValueError:
+                        pass
+
+                    raise ValueError('Invalid input, could not parse')
+                
+                start_datetime = start_and_end_datetimes[0]
+                end_datetime = start_and_end_datetimes[1]
+                start_time_in_minutes = (start_datetime.hour*60
+                                            + start_datetime.minute)
+                end_time_in_minutes = (end_datetime.hour*60
+                                        + end_datetime.minute)
+                if (start_time_in_minutes > end_time_in_minutes):
+                    raise ValueError('End time is earlier than start '
+                                        'time')
+
+                for shift in parsed_shift_datetimes:
+                    other_start = (shift[0].hour*60 
+                                    + shift[0].minute)
+                    other_end = (shift[1].hour*60 
+                                    + shift[1].minute)
+                    if ((end_time_in_minutes > other_start
+                            and start_time_in_minutes < other_end)
+                        or (start_time_in_minutes < other_end
+                            and end_time_in_minutes > other_start)):
+                        raise ValueError('Invalid input, as '
+                                            'scheduled times overlap')
+
+                parsed_shift_datetimes.append(start_and_end_datetimes)
+
+            complete_day = [[datetime.datetime.strftime(time, "%H:%M")
+                                for time in shift]
+                                for shift in parsed_shift_datetimes]
+            user_schedule[current_day] = complete_day
+
+        return user_schedule
 
     @tasks.loop(minutes=1.0)
     async def update_scheduled_roles(self):
